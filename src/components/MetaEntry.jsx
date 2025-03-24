7581f7348f76efafc662bd17d16b72a9f159583f
rtm516 <rtm516@users.noreply.github.com>
2025-03-24 15:45:26 +0000

    Make hashes monospaced

--- src/components/MetaEntry.jsx
+++ src/components/MetaEntry.jsx
@@ -6,8 +6,8 @@ function MetaEntry ({ data, onShowDetails }) {
       <th scope='row'>{data.version}</th>
       <td>{data.archive}</td>
       <td>{data.arch}</td>
-      <td>{data.md5}</td>
-      <td>{data.sha256}</td>
+      <td class="font-monospace">{data.md5}</td>
+      <td class="font-monospace">{data.sha256}</td>
       <td><button type='button' className='btn btn-primary' onClick={() => onShowDetails(data)}><i className='fa-solid fa-circle-info' /></button></td>
     </tr>
   )
